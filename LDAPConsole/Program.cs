e24fda90c898471ee559d9c6e6cef51ec8c9e81d
Derek Hubbard <derek.hubbard@pomiet.com>
2014-05-28 11:41:57 -0400

    Initial commit.

---
+++ LDAPConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Novell.Directory.Ldap;
+
+namespace LDAPConsole
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Connecting to LDAP...");
+
+			var ldapService = new LDAPDirectoryService () {
+				Host = "host"
+			};	
+
+			ldapService.Login ("un", "pwd");
+		}
+	}
+
+	public class LDAPDirectoryService 
+	{
+		public string Host { get; set; }
+
+
+		public int Port { get; set; }
+
+		LdapConnection conn = null;
+
+		public LDAPDirectoryService()
+		{
+			Host = string.Empty;
+			Port = 389;
+		}
+
+		public void Login(string username, string password)
+		{
+			this.conn = new LdapConnection ();
+			conn.Connect (Host, Port);
+
+			conn.Bind (username, password);
+
+			Console.WriteLine ("Connected");
+		}
+
+		public void CheckPassword(string testUsername, string testPassword)
+		{
+
+			var attr = new LdapAttribute("userPassword", testPassword);
+			bool correct = conn.Compare(testUsername, attr);
+
+			System.Console.Out.WriteLine(correct?"The password is correct.":"The password is incorrect.\n");
+
+			// disconnect with the server
+			conn.Disconnect();
+		}
+	}
+}
